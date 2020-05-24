f0f3d2a246100a87af4fcfd05a0634413b4e5819
Carlos López <yo@carlop.es>
2020-05-24 20:08:05 +0200

    ✨Random colors for tetrominoes

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let nextRandom = 0
     let timerId
     let score = 0
+    const colors = [
+        '#FF8552',
+        '#39393A',
+        '#85ffc7',
+        '#297373',
+        '#39393A',
+    ]
 
     console.log(squares)
 
@@ -62,6 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
+            squares[currentPosition + index].style.backgroundColor = colors[random]
         })
     }
 
@@ -69,6 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function unDraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
+            squares[currentPosition + index].style.backgroundColor = ''
         })
     }
 
@@ -155,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Show up-next tetromino in mini-grid display
     const displaySquares = document.querySelectorAll('.mini-grid div')
     const displayWidth = 4
-    let displayIndex = 0
+    const displayIndex = 0
 
     // The Tetrominoes without rotations
     const upNextTetrominoes = [
@@ -171,9 +180,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // remove any trace of a tetromino form the entire grid
         displaySquares.forEach(square => {
             square.classList.remove('tetromino')
+            square.style.backgroundColor = ''
         })
         upNextTetrominoes[nextRandom].forEach(index => {
             displaySquares[displayIndex + index].classList.add('tetromino')
+            displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
         })
     }
 
@@ -202,6 +213,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 row.forEach(index => {
                     squares[index].classList.remove('taken')
                     squares[index].classList.remove('tetromino')
+                    squares[index].style.backgroundColor = ''
                 })
                 const squaresRemoved = squares.splice(i, width)
                 squares = squaresRemoved.concat(squares)
