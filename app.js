2eaa5cde97300bad55e4aa455223fa090d4bece9
Carlos López <yo@carlop.es>
2020-05-24 18:03:09 +0200

    ✨Loaded grid, buttons and score

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMDocumentLoaded', () => {
-    console.log("Game initialited");
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Game initialized')
+
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startButton = document.querySelector('#start-button')
+    const width = 10
+
+    console.log(squares)
+
 })
